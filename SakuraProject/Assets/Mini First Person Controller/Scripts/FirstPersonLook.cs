ee937bb96e60285a178647df8a4e1ddaad82ec79
Marina Ramírez <chaips02@gmail.com>
2024-08-25 19:00:17 +0200

    transicion inicio

--- SakuraProject/Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
+++ SakuraProject/Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -6,6 +6,8 @@ public class FirstPersonLook : MonoBehaviour
     Transform character;
     public float sensitivity = 2;
     public float smoothing = 1.5f;
+    private float _time = 0f;
+    private const float _allowRotationTime = 2f;
 
     Vector2 velocity;
     Vector2 frameVelocity;
@@ -27,15 +29,26 @@ public class FirstPersonLook : MonoBehaviour
 
     void Update()
     {
-        // Get smooth velocity.
-        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
-        frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
-        velocity += frameVelocity;
-        velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+        CameraRotation();
+    }
 
-        // Rotate camera up-down and controller left-right from velocity.
-        transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
-        character.localRotation = Quaternion.Euler(0, velocity.x + 180, 0); // keep the initial 180 degrees rotation
+    private void CameraRotation()
+    {
+        if (_time >= _allowRotationTime)
+        {
+            // Get smooth velocity.
+            Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
+            frameVelocity = Vector2.Lerp(frameVelocity, rawFrameVelocity, 1 / smoothing);
+            velocity += frameVelocity;
+            velocity.y = Mathf.Clamp(velocity.y, -90, 90);
+
+            // Rotate camera up-down and controller left-right from velocity.
+            float lerp = _time < _allowRotationTime * 3f ? Time.deltaTime : 1f;
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.AngleAxis(-velocity.y, Vector3.right), lerp);
+            character.localRotation = Quaternion.Euler(0, velocity.x + 180, 0); // keep the initial 180 degrees rotation
+        }
+        
+        _time += Time.deltaTime;
     }
 }
